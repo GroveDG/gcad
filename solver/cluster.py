039e41c0fb287a30ef9ad1cbadd9ce74994fd6b0
GroveDG <grovedegroof@gmail.com>
2024-11-07 19:49:34 -0500

    rule_4

--- solver/cluster.py
+++ solver/cluster.py
@@ -3,6 +3,7 @@ from enum import Enum, auto
 from typing import Self
 import numpy as np
 from points import Points
+from copy import copy
 
 class ClusterType(Enum):
 	Rigid = auto()
@@ -30,7 +31,7 @@ class Cluster():
 def rule_1(c1: Cluster, c2: Cluster) -> Cluster:
 	assert c1.type == ClusterType.Rigid and c2.type == ClusterType.Rigid
 
-	c1.points.map_to(c2.points)
+	c2.points.map_to(c1.points)
 	
 	result = c1 | c2
 
@@ -39,7 +40,7 @@ def rule_1(c1: Cluster, c2: Cluster) -> Cluster:
 def rule_2(c1: Cluster, c2: Cluster) -> Cluster:
 	assert c1.type == ClusterType.Radial and c2.type == ClusterType.Radial
 
-	c1.points.map_to(c2.points, do_scale=True)
+	c2.points.map_to(c1.points, do_scale=True)
 	
 	result = c1 | c2
 
@@ -48,8 +49,29 @@ def rule_2(c1: Cluster, c2: Cluster) -> Cluster:
 def rule_3(c1: Cluster, c2: Cluster) -> Cluster:
 	assert c1.type == ClusterType.Scalable and c2.type == ClusterType.Scalable
 
-	c1.points.map_to(c2.points, do_scale=True)
+	c2.points.map_to(c1.points, do_scale=True)
 	
 	result = c1 | c2
+
+	return result
+
+def rule_4(c1: Cluster, c2: Cluster, c3: Cluster) -> Cluster:
+	assert c1.type == ClusterType.Rigid and c2.type == ClusterType.Rigid and c3.type == ClusterType.Rigid
+
+	# The index of the point corresponds to the cluster it is NOT in.
+	# ex: shared_points[0] is not in c1
+	shared_points = [
+		c2.points.ids.intersection(c3.points.ids),
+		c3.points.ids.intersection(c1.points.ids),
+		c1.points.ids.intersection(c2.points.ids)
+	]
+
+	for i in range(3):
+		assert len(shared_points[i]) == 1
+		shared_points[i] = shared_points[i][0]
+
+	result = copy(c1)
+	result[shared_points[0]]
+	
 
 	return result
