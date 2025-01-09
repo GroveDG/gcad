1b756cb4a1cb6d2a05e1009e1f4b17e3c016640d
grovedgdev@gmail.com <grovedegroof@gmail.com>
2025-01-08 23:33:31 -0500

    Currect ordering.

--- src/order/bfs_order.rs
+++ src/order/bfs_order.rs
@@ -8,39 +8,60 @@ use crate::constraints::{elements::Point, Constraint};
 
 use super::{print_tree, PointIndex};
 
+fn add_constraints<'a>(
+    index: &'a PointIndex,
+    points: &HashSet<&Point>,
+    point: &Point,
+    support: &mut MultiMap<&'a Point, &'a Box<dyn Constraint>>
+) -> Vec<&'a Point> {
+    // For all constraints containing this point...
+    index.get_constraints(point).into_iter().map(|c| {
+        // for all valid targets of this constraint...
+        c.targets(&points).into_iter().filter_map(|t| {
+            // Get current number of constraints on target.
+            let l = support.get_vec(t)
+                .map(|v| v.len())
+                .unwrap_or(0);
+            debug_assert!(l <= 2);
+            // Skip those already discrete.
+            if l == 2 { return None; }
+            // Add constraint to target.
+            support.insert(t, c);
+            // If target is now discrete...
+            if l+1 < 2 { return None; }
+            // return as known.
+            Some(t)
+        }).collect::<Vec<&Point>>()
+    }).flatten().unique().collect()
+}
+
 pub fn compute_tree<'a>(
     root: &'a Point,
     orbiter: &'a Point,
     index: &'a PointIndex,
 ) -> (Tree<&'a Point>, HashSet<&'a Point>) {
-    let mut points: HashSet<&Point> = HashSet::from_iter([root, orbiter]);
+    let mut support = MultiMap::new();
+    let mut points: HashSet<&Point> = HashSet::from_iter([root]);
+    add_constraints(index, &points, root, &mut support);
+    points.insert(orbiter);
     let mut tree = TreeBuilder::new().with_root(root).build();
     let mut queue = VecDeque::from_iter([
-        (root, tree.root_id().unwrap()),
         (orbiter, tree.root_mut().unwrap().append(orbiter).node_id())
     ]);
     // Supporting constraints.
     // Any constraints which can be applied to a point for a given set of
     // previous points.
     // If any point has 2 or more constraints it is assumed to be discrete.
-    let mut support = MultiMap::new();
     while !queue.is_empty() {
-        let (p, node_id) = queue.pop_front().unwrap();
-        for c in index.get_constraints(p) {
-            for k in c.targets(&points) {
-                // TODO
-                // Replace with a rigorous discreteness test.
-                let l = support.get_vec(k)
-                    .map(|v| v.len())
-                    .unwrap_or(0);
-                debug_assert!(l <= 2);
-                if l == 2 { continue }
-                support.insert(k, c);
-                if l+1 < 2 { continue }
-                points.insert(k);
-                queue.push_back((k, node_id));
-                tree.get_mut(node_id).unwrap().append(k);
-            }
+        let (point, node_id) = queue.pop_front().unwrap();
+        let next = add_constraints(index, &points, point, &mut support);
+        for p in next {
+            // Mark as known.
+            points.insert(p);
+            // Queue after all current points.
+            queue.push_back((p, node_id));
+            // Add as child.
+            tree.get_mut(node_id).unwrap().append(p);
         }
     }
     println!("{:?}", support);
