7f664298e3a1db68333df96fbf90a04ecbe483e5
grovedgdev@gmail.com <grovedegroof@gmail.com>
2025-01-13 00:12:21 -0500

    root_pairs and order type change

--- src/order/bfs.rs
+++ src/order/bfs.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 use itertools::Itertools;
 use multimap::MultiMap;
@@ -47,14 +47,13 @@ fn add_constraints<'a>(
         .collect()
 }
 
-pub fn compute_tree<'a>(
+fn compute_tree<'a>(
     root: &'a Point,
     orbiter: &'a Point,
     index: &'a PointIndex,
 ) -> (
-    Vec<&'a Point>,
+    Vec<(&'a Point, Vec<&'a dyn Constraint>)>,
     HashSet<&'a Point>,
-    MultiMap<&'a Point, &'a dyn Constraint>,
 ) {
     let mut support = MultiMap::new();
     let mut points: HashSet<&Point> = HashSet::from_iter([root]);
@@ -77,79 +76,71 @@ pub fn compute_tree<'a>(
         }
         i += 1;
     }
-    (order, points, support)
+    (
+        order
+            .into_iter()
+            .map(|p| (p, support.remove(p).unwrap_or_default()))
+            .collect(),
+        points,
+    )
 }
 
-pub fn compute_forest(index: &PointIndex) -> Vec<Vec<(Point, Vec<&dyn Constraint>)>> {
-    let mut forest: Vec<(
-        Vec<&Point>,
-        HashSet<&Point>,
-        MultiMap<&Point, &dyn Constraint>,
-    )> = Vec::new();
-    for [root, orbiter] in index
-        // Iterate through points...
-        .get_points()
+fn root_pairs<'a>(index: &'a PointIndex) -> impl Iterator<Item = (&'a Point, &'a Point)> {
+    let mut neighbors = HashMap::new();
+    for p in index.get_points() {
+        let known_points = HashSet::from_iter([p]);
+        let targets = index
+            .get_constraints(p)
+            .unwrap()
+            .into_iter()
+            // Get valid targets given only this root.
+            .map(|c| c.targets(&known_points))
+            // Only unique targets.
+            .flatten()
+            .unique()
+            // Exclude pairs already found reversed.
+            .filter(|t| {
+                neighbors
+                    .get(t)
+                    .is_some_and(|n: &Vec<&String>| n.contains(&p))
+            })
+            .collect::<Vec<&Point>>();
+        neighbors.insert(p, targets);
+    }
+    neighbors
         .into_iter()
-        // as roots of potential trees.
-        .map(|root| {
-            // Given this point (root) as the only known point...
-            let known_points = HashSet::from_iter([root]);
-            index
-                // iterate through the root's constraints...
-                .get_constraints(root)
-                .unwrap()
-                .into_iter()
-                // get the points which can be constrained...
-                .map(move |c| c.targets(&known_points))
-                // combine these sets of points...
-                .flatten()
-                // get only the unique points...
-                .unique()
-                // and map them in pairs with the root.
-                .map(move |orbiter| {
-                    let mut ps = [root, orbiter];
-                    // (sorted to detect when the orbiter is the root)
-                    ps.sort();
-                    ps
-                })
-        })
-        // Get only unique [root, orbiter] pairs.
+        .map(|(p, targets)| [p].into_iter().cycle().zip(targets.into_iter()))
         .flatten()
-        .unique()
-    {
+}
+
+fn compute_forest(index: &PointIndex) -> Vec<Vec<(&Point, Vec<&dyn Constraint>)>> {
+    let mut forest: Vec<(
+        Vec<(&Point, Vec<&dyn Constraint>)>, // order
+        HashSet<&Point>,                     // contained
+    )> = Vec::new();
+    for (root, orbiter) in root_pairs(index) {
         // If this root pair is contained in any tree, skip it.
         if forest
             .iter()
-            .any(|(_, p, _)| p.contains(root) && p.contains(orbiter))
+            .any(|(_, p)| p.contains(root) && p.contains(orbiter))
         {
             continue;
         }
         // Compute this pair's tree.
-        let (order, points, support) = compute_tree(root, orbiter, &index);
+        let (order, points) = compute_tree(root, orbiter, &index);
         // Iterate through trees...
         forest = forest
             .into_iter()
             // and discard any contained by this new tree.
-            .filter(|(_, p, _)| !points.is_superset(p))
+            .filter(|(_, p)| !points.is_superset(p))
             .collect();
         // Add this new tree.
-        forest.push((order, points, support));
+        forest.push((order, points));
     }
 
-    forest
-        .into_iter()
-        .map(|(t, _, mut s)| {
-            t.into_iter()
-                .map(|p| (p.clone(), s.remove(p).unwrap_or_else(|| Vec::new())))
-                .collect::<Vec<(Point, Vec<&dyn Constraint>)>>()
-        })
-        .collect::<Vec<Vec<(Point, Vec<&dyn Constraint>)>>>()
+    forest.into_iter().map(|(t, _)| t).collect()
 }
 
-pub fn bfs_order(index: &PointIndex) -> Vec<(Point, Vec<&dyn Constraint>)> {
-    let mut o = Vec::new();
-    for mut t in compute_forest(&index) {
-        o.append(&mut t);
-    }
-    o
+pub fn bfs_order(index: &PointIndex) -> Vec<(&Point, Vec<&dyn Constraint>)> {
+    compute_forest(&index).concat()
 }
--- src/solve/brute.rs
+++ src/solve/brute.rs
@@ -3,19 +3,19 @@ use std::collections::HashMap;
 use crate::{constraints::{elements::Point, Constraint}, math::{geo::{choose, meet, Geo}, vector::Vector}};
 
 fn solve_iter(
-    order: &Vec<(Point, Vec<&dyn Constraint>)>,
+    order: &Vec<(&Point, Vec<&dyn Constraint>)>,
     positions: &mut HashMap<Point, Vector>,
     i: usize
 ) -> Result<(), ()> {
     let Some((point, cs)) = order.get(i) else { return Ok(()) };
     let mut geo = vec![Geo::All];
     for &c in cs {
-        let i = c.points().iter().position(|v| v==point).unwrap();
+        let i = c.points().iter().position(|v| v==*point).unwrap();
         let c_geo = c.to_geo(&positions, i);
         geo = meet(geo, c_geo);
     }
     for g in geo {
-        positions.insert(point.clone(), choose(g));
+        positions.insert((*point).clone(), choose(g));
         if solve_iter(order, positions, i+1).is_ok() {
             return Ok(());
         }
@@ -23,7 +23,7 @@ fn solve_iter(
     return Err(());
 }
 
-pub fn brute_solve(order: Vec<(Point, Vec<&dyn Constraint>)>) -> Result<HashMap<Point, Vector>, String>{
+pub fn brute_solve(order: Vec<(&Point, Vec<&dyn Constraint>)>) -> Result<HashMap<Point, Vector>, String>{
     let mut positions = HashMap::new();
     if solve_iter(&order, &mut positions, 0).is_ok() {
         Ok(positions)
