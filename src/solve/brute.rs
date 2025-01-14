3671444734884c5ab2b52d0babf692a5659220da
grovedgdev@gmail.com <grovedegroof@gmail.com>
2025-01-13 21:42:26 -0500

    brute cleanup

--- src/solve/brute.rs
+++ src/solve/brute.rs
@@ -1,29 +1,39 @@
 use std::collections::HashMap;
 
-use crate::{constraints::{elements::Point, Constraint}, math::{geo::{choose, meet, Geo}, vector::Vector}};
+use crate::{
+    constraints::{elements::Point, Constraint},
+    math::{
+        geo::{choose, meet, Geo},
+        vector::Vector,
+    },
+};
 
 fn solve_iter(
     order: &Vec<(&Point, Vec<&dyn Constraint>)>,
     positions: &mut HashMap<Point, Vector>,
-    i: usize
+    i: usize,
 ) -> Result<(), ()> {
-    let Some((point, cs)) = order.get(i) else { return Ok(()) };
+    let Some((point, cs)) = order.get(i) else {
+        return Ok(());
+    };
     let mut geo = vec![Geo::All];
     for &c in cs {
-        let i = c.points().iter().position(|v| v==*point).unwrap();
+        let i = c.points().iter().position(|v| v == *point).unwrap();
         let c_geo = c.to_geo(&positions, i);
         geo = meet(geo, c_geo);
     }
     for g in geo {
         positions.insert((*point).clone(), choose(g));
-        if solve_iter(order, positions, i+1).is_ok() {
+        if solve_iter(order, positions, i + 1).is_ok() {
             return Ok(());
         }
     }
     return Err(());
 }
 
-pub fn brute_solve(order: Vec<(&Point, Vec<&dyn Constraint>)>) -> Result<HashMap<Point, Vector>, String>{
+pub fn brute_solve(
+    order: Vec<(&Point, Vec<&dyn Constraint>)>,
+) -> Result<HashMap<Point, Vector>, String> {
     let mut positions = HashMap::new();
     if solve_iter(&order, &mut positions, 0).is_ok() {
         Ok(positions)
